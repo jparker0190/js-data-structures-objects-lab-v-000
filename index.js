39d9dee4bd532ad2fa84d1ebbdeeb504023518be
Joseph Parker <jparker010190@gmail.com>
2018-07-07 19:16:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 function updateDriverWithKeyAndValue(driver, key, value){
-  const new = {...driver}
-  new[key] = value
-  return new
+  const neww = {...driver}
+  neww[key] = value
+  return neww
 }
