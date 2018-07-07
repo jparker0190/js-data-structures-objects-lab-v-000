48752e76b20614be5579a1182b5a90545aa16f54
Joseph Parker <jparker010190@gmail.com>
2018-07-07 19:16:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 function updateDriverWithKeyAndValue(driver, key, value){
-  driver[key] = value
-  return driver
-  console.log(typeof driver)
+  const new = {...driver}
+  new[key] = value
+  return new
 }
