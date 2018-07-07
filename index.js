8878ba6f861c66772448900cd2d8f542af9a659d
Joseph Parker <jparker010190@gmail.com>
2018-07-07 19:29:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   return driver
 }
 function deleteFromDriverByKey(driver, key){
-  delete driver.key
+  driver.pop(key)
 }
 function destructivelyDeleteFromDriverByKey(driver, key){
   delete driver.key
